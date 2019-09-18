132e2e20dcb8ca6d1bcade8295feec453f6fa5f1
HazardWorks <domelement0001@gmail.com>
2019-09-18 11:24:09 -0400

    Added "config.json" to "buildpug.js"

--- buildpug.js
+++ buildpug.js
@@ -2,6 +2,8 @@ const fs            = require('fs');
 const path          = require('path');
 const pug           = require('pug');
 
+const config        = require('./config.json');
+
 const sourcePath    = path.join(__dirname, 'source', 'views');
 const buildPath     = path.join(__dirname);
 
@@ -14,7 +16,23 @@ fs.readdir(sourcePath, function(error, files) {
 
             let compile = pug.compileFile(filePath);
 
-            let htmlCode = compile({});
+            let compileOptions = {
+                title: config.title,
+                description: config.description,
+
+                cdnFontAwesome: config.cdn.fontAwesome,
+                cdnBootstrapCss: config.cdn.bootstrapCss,
+                cdnBootstrapJs: config.cdn.bootstrapJs,
+                cdnJQuery: config.cdn.jQuery,
+
+                buttons: []
+            }
+            let buttonKeys = Object.keys(config.buttons);
+            buttonKeys.forEach((key) => {
+                compileOptions.buttons.push(config.buttons[key]);
+            })
+
+            let htmlCode = compile(compileOptions);
 
             fs.writeFileSync(newFilePath, htmlCode);
         }
